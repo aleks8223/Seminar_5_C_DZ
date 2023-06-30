1f9abbc1ea9713ae0d146a951020887167b1f40f
aleks8223 <aleks8223@hotmail.com>
2023-06-30 23:05:03 +0600

    Добавлено задача 36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных ИНДЕКСАХ.
+
+Console.Write("Введите количество элементов массива ");
+int a = int.Parse(Console.ReadLine()!);
+int[] randomArray = new int[a];
+
+void array(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+          randomArray[i] = new Random().Next(1,9);
+     Console.Write(randomArray[i]+",");
+    }
+}
+
+int quantity(int[] randomArray)
+{
+    int sum = 0;
+    int i = 0;
+    while (i < randomArray.Length)
+    {
+        sum = sum + randomArray[i];
+        i = i + 2;
+    }
+    return sum;
+}
+
+array(a);
+Console.Write($" Сумма элементов, на нечётных индексах: {quantity(randomArray)}");
